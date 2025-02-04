4941a62576957ee19d026238e86cfbb98e6df720
Siori9 <clara.layus@hotmail.fr>
2025-02-04 15:28:29 +0100

    fix(front): fix build

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import TournamentCreation from "./components/TournamentCreation.vue";
+// import UserPage from "./components/UserPage.vue";
 </script>
 
 <template>
   <TournamentCreation></TournamentCreation>
+<!--  <UserPage />-->
 </template>
 
 <style scoped>
